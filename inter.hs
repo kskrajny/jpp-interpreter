656731ec9f37ddfa24ef9a6b6f7f739b671cd727
kuskraju <kuskrau@gmail.com>
2021-04-09 13:24:45 +0200

    nice

--- inter.hs
+++ inter.hs
@@ -1,110 +1,147 @@
 {-# LANGUAGE DataKinds #-}
 import Data.Function
 import Data.List
-import Data.Map ( (!), findMax, insert, null, singleton, Map, fromList )
+import Data.Map ( (!), findMax, insert, null, singleton, Map, fromList, empty )
 
 type Var = String
 type Loc = Int
-type Store = Map Loc Int
+type Store = Map Loc Val
 type VEnv = Map String Loc
+type Proc = Val -> Store -> Store
+type FEnv = Map String Proc
 
 infix 1 :=
 
+data Val
+  = String String
+  | Int Int
+  | Bool Bool
+  deriving (Show, Eq, Ord)
+
+andVal :: Val -> Val -> Val
+andVal (Bool a) (Bool b) = Bool (a && b)
+andVal a b = undefined
+orVal :: Val -> Val -> Val
+orVal (Bool a) (Bool b) = Bool (a || b) 
+orVal a b = undefined
+notVal :: Val -> Val
+notVal (Bool a) = Bool (not a)
+notVal a = undefined
+addVal :: Val -> Val -> Val
+addVal (Int a) (Int b) = Int (a + b)
+addVal (String a) (String b) = String (a ++ b)
+subVal :: Val -> Val -> Val
+subVal (Int a) (Int b) = Int (a - b)
+mulVal :: Val -> Val -> Val
+mulVal (Int a) (Int b) = Int (a * b)
+divVal :: Val -> Val -> Val
+divVal (Int a) (Int b) = Int (div a b)
+castBool :: Val -> Bool
+castBool (Bool a) = a
+castBool a = undefined
+
 data Exp
-  = N Int           -- constant                                                    
-  | V Var           -- variable                                                     
+  = V Var
+  | C Val
   | Add Exp Exp     -- addition                                                     
   | Sub Exp Exp     -- subtraction                                                  
   | Mul Exp Exp     -- multiplication                                               
---  | Div Exp Exp     -- division
-
-data BExp
-  = B Bool              -- variable 
+  | Div Exp Exp     -- division
   | Equal Exp Exp       -- equal
   | Less Exp Exp        -- less
-  | Or BExp BExp        -- or
-  | And BExp BExp       -- and
-  | Not BExp            -- not
+  | Or Exp Exp        -- or
+  | And Exp Exp       -- and
+  | Not Exp            -- not
 
 newtype Ref = Ref Var             -- referance 
 
-data Arg
-  = EmptyArg
-  | RefArg Ref Arg
-  | VarArg Var Arg
-
 data Decl
   = DeclVar Var Decl               -- variable declaration
-  | DeclFunc Var Arg Stmt Decl     -- function declaration
+  | DeclFunc Var Var Stmt Decl     -- function declaration
   | EmptyDecl
 
 data Stmt
   = Var := Exp                  -- assignment                                                
   | SeqStmt Stmt Stmt           -- sequence
-  | IfStmt BExp Stmt            -- if
-  | IfElseStmt BExp Stmt Stmt   -- if else
-  | WhileStmt BExp Stmt         -- while
+  | IfStmt Exp Stmt            -- if
+  | IfElseStmt Exp Stmt Stmt   -- if else
+  | WhileStmt Exp Stmt         -- while
   | SkipStmt                    -- skip
   | BlockStmt Decl Stmt         -- block statement
+  | Func Var Exp Var            -- function 
 
 newloc :: Store -> Loc
 newloc s = if Data.Map.null s
            then 0
            else fst(findMax s) + 1
 
-semE :: Exp -> VEnv -> Store -> Int
-semE (N x) v s = x
+semE :: Exp -> VEnv -> Store -> Val
+semE (C x) v s = x
 semE (V x) v s = s ! (v ! x)
-semE (Add e1 e2) v s = semE e1 v s + semE e2 v s
-semE (Sub e1 e2) v s = semE e1 v s - semE e2 v s
-semE (Mul e1 e2) v s = semE e1 v s * semE e2 v s
--- semE (e1 :/: e2) v s = semE e1 v s / semE e2 v s
-
-semB :: BExp -> VEnv -> Store -> Bool
-semB (B b) v s = b
-semB (Equal e1 e2) v s = semE e1 v s == semE e2 v s
-semB (Less e1 e2) v s = semE e1 v s < semE e2 v s
-semB (Or b1 b2) v s = semB b1 v s || semB b2 v s
-semB (And b1 b2) v s = semB b1 v s && semB b2 v s
-semB (Not b) v s = not (semB b v s)
+semE (Add e1 e2) v s = addVal (semE e1 v s) (semE e2 v s)
+semE (Sub e1 e2) v s = subVal (semE e1 v s) (semE e2 v s)
+semE (Mul e1 e2) v s = mulVal (semE e1 v s) (semE e2 v s)
+semE (Div e1 e2) v s = divVal (semE e1 v s) (semE e2 v s)
+semE (Equal e1 e2) v s = Bool (semE e1 v s == semE e2 v s)
+semE (Less e1 e2) v s = Bool (semE e1 v s < semE e2 v s)
+semE (Or e1 e2) v s = orVal (semE e1 v s) (semE e2 v s)
+semE (And e1 e2) v s = andVal (semE e1 v s) (semE e2 v s)
+semE (Not e) v s = notVal (semE e v s)
 
 --TODO FUNC
-semD :: Decl -> VEnv -> Store -> (VEnv, Store)
-semD EmptyDecl v s = (v, s)
-semD (DeclVar x d) v s =
-  semD d vd sd where
+semD :: Decl -> VEnv -> FEnv -> Store -> (VEnv, FEnv, Store)
+semD EmptyDecl v f s = (v, f, s)
+semD (DeclVar x d) v f s =
+  semD d vd f sd where
     vd = Data.Map.insert x l v
-    sd = Data.Map.insert l 0 s
+    sd = Data.Map.insert l (Int 0) s
     l = newloc s
-semD (DeclFunc x a s1 d) v s = (v, s)
+semD (DeclFunc x a s1 d) v f s = semD d v fd s where
+  fd = Data.Map.insert x func f
+  l = newloc s
+  func arg s = semS s1 vf fd sf where
+    vf = Data.Map.insert x l v
+    sf = Data.Map.insert l arg s
 
-semS :: Stmt -> VEnv -> Store -> Store
-semS SkipStmt v s = s
-semS (x := e) v s = Data.Map.insert (v ! x) (semE e v s) s
-semS (SeqStmt s1 s2) v s = semS s2 v (semS s1 v s)
-semS (IfStmt b s1) v s =
-       if semB b v s then semS s1 v s else s
-semS (IfElseStmt b s1 s2) v s =
-       if semB b v s then semS s1 v s else semS s2 v s
-semS (WhileStmt b s1) v s =
-       if semB b v s
-       then semS (WhileStmt b s1) v (semS s1 v s)
+semS :: Stmt -> VEnv -> FEnv -> Store -> Store
+semS SkipStmt v f s = s
+semS (x := e) v f s = Data.Map.insert (v ! x) (semE e v s) s
+semS (SeqStmt s1 s2) v f s = semS s2 v f (semS s1 v f s)
+semS (IfStmt e s1) v f s =
+       if castBool (semE e v s) then semS s1 v f s else s
+semS (IfElseStmt e s1 s2) v f s =
+       if castBool (semE e v s) then semS s1 v f s else semS s2 v f s
+semS (WhileStmt e s1) v f s =
+       if castBool (semE e v s)
+       then semS (WhileStmt e s1) v f (semS s1 v f s)
        else s
-semS (BlockStmt d s1) v s =
-        semS s1 vd sd where
-          (vd, sd) = semD d v s
+semS (BlockStmt d s1) v f s =
+        semS s1 vd fd sd where
+          (vd, fd, sd) = semD d v f s
 
-powerStmt :: Stmt
-powerStmt =
-  WhileStmt (Less (V "i") (V "n")) (
-    SeqStmt
-      ("x" := Mul (V "x") (V "c"))
-      ("i" := Add (V "i") (N 1))
-  )
+powerStmt :: Int -> Int -> Stmt
+powerStmt a b =
+  BlockStmt
+    (DeclVar "x"
+      (DeclVar "i"
+        (DeclVar "n"
+          (DeclVar "c" EmptyDecl))))
+    (SeqStmt
+      (SeqStmt ("x" := C (Int 1))
+        (SeqStmt ("i" := C (Int 0))
+          (SeqStmt ("n" := C (Int b)) ("c" := C (Int a)))))
+      (IfElseStmt
+        (Less (V "n") (C (Int 0)))
+        ("x" := C (Int (-1)))
+        (WhileStmt
+          (Less (V "i") (V "n"))
+          (SeqStmt
+            ("x" := Mul (V "x") (V "c"))
+            ("i" := Add (V "i") (C (Int 1)))))))
 
-power :: Int -> Int -> Int
+
+power :: Int -> Int -> Val
+
 power a b = x where
-  store = semS powerStmt
-    (Data.Map.fromList [("x", 0), ("i", 1), ("n", 2), ("c", 3)])
-    (Data.Map.fromList [(0, 1), (1, 0), (2, b), (3, a)])
+  store = semS (powerStmt a b) Data.Map.empty Data.Map.empty Data.Map.empty
   x = store ! 0
